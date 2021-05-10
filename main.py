5a69678047ade9a60e1f90fdd9641185909d74ed
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-10 12:38:12 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+contacts = {
+    "Jame" :
+        {
+            "phone_number" : 123455667
+            "email" :  "jame.le@gmail.com"
+        },
+    "kile" :
+        {
+            "phone_number" : 34452221111
+            "email"  : "kilenguyen@yahoo.com"
+        }
+}
+print(contacts["james"])
